5205cc2cebe59e68c5399bf9e7baa6c227c81039
mbme <stribog.ua@gmail.com>
2014-09-15 15:25:14 +0300

    simple UI

--- src-cljs/vita/app.cljs
+++ src-cljs/vita/app.cljs
@@ -9,20 +9,42 @@
                       :records (gen/random-records 10)}
                      ))
 
+(q/defcomponent SearchBox []
+  (d/div {:className "search-box"}
+         "Search:" (d/input {:type "text"})
+         ))
 
-(q/defcomponent Record [{:keys [name data]}]
+(q/defcomponent SearchResult [record]
+  (d/div {:className "search-result"} (:name record)))
+
+(q/defcomponent SearchResults [records]
+  (apply d/div {:className "search-results"}
+         (map SearchResult records)
+         ))
+
+(q/defcomponent ControlPanel [records]
+  (d/div {:id "control-panel"} (SearchBox) (SearchResults records)))
+
+(q/defcomponent Preview [component data]
+  (d/div {:id "preview"} (if (nil? data)
+                           (d/h3 {} "no data :(")
+                           (component data)
+                           )))
+
+(q/defcomponent Record[{:keys [name data]}]
   (d/div {:className "record" :key (hash name)}
          (d/h2 {} name)
          (d/div {} data)
          ))
 
 (q/defcomponent Root [records]
-  (apply d/div {:className "records"}
-         (map #(Record %) records)
+  (d/div {:id "root"}
+         (ControlPanel records)
+         (Preview Record (first records))
          ))
 
 (defn render [data]
-  (q/render (Root (:records @data)) (.getElementById js/document "main")) )
+  (q/render (Root (:records @data)) js/document.body))
 
 ;; listen for changes in state and call render
 (add-watch state :render
