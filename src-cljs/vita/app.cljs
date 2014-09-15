5fd5b5d83eb014e2721a5375105f168550cc0fda
mbme <stribog.ua@gmail.com>
2014-09-15 23:12:25 +0300

    Highlight selected record

--- src-cljs/vita/app.cljs
+++ src-cljs/vita/app.cljs
@@ -15,7 +15,7 @@
 
 (defonce state (atom {:records (gen/random-records 10)
                       :search-term ""
-                      :selected-rec-name nil}))
+                      :selected-rec nil}))
 
 (defn- update-search
   "If `term' really changed then update state."
@@ -25,9 +25,9 @@
     (swap! state assoc :search-term term)))
 
 (defn- update-selected-rec [name]
-  (when-not (= name (:selected-rec-name @state))
+  (when-not (= name (:selected-rec @state))
     (log/info "selected record: %s" name)
-    (swap! state assoc :selected-rec-name name)))
+    (swap! state assoc :selected-rec name)))
 
 (defn- get-record [records name]
   (if-not (nil? name) (->> records
@@ -36,6 +36,18 @@
 
 (defn- filter-records [records term]
   (filter #(has-term? % term) records))
+
+(defn- mark-selected-record [records selected-rec]
+  (map #(if (= (:name %) selected-rec)
+          (assoc % :selected true)
+          %)
+       records))
+
+(defn class-name [& all]
+  (->> all
+       (remove nil?)
+       (interpose " ")
+       (apply str)))
 
 ;; COMPONENTS PART
 
@@ -46,8 +58,8 @@
                              :onKeyUp #(update-search (e-val %))})
          ))
 
-(q/defcomponent SearchResult [{:keys [name]}]
-  (d/div {:className "search-result"
+(q/defcomponent SearchResult [{:keys [name selected]}]
+  (d/div {:className (class-name "search-result" (when selected "selected"))
           :data-key name
           :onClick #(update-selected-rec (e-key %))} name))
 
@@ -57,10 +69,14 @@
            (list (d/div {} "0 results"))
            (map SearchResult records))))
 
-(q/defcomponent ControlPanel [{:keys [records search-term]}]
+(q/defcomponent ControlPanel [{:keys [records search-term selected-rec]}]
   (d/div {:id "control-panel"}
          (SearchBox search-term)
-         (SearchResults (filter-records records search-term))))
+         (-> records
+             (filter-records search-term)
+             (mark-selected-record selected-rec)
+             (SearchResults))
+         ))
 
 (q/defcomponent Preview [data component]
   (d/div {:id "preview"} (if-not (nil? data) (component data))))
@@ -73,7 +89,7 @@
 (q/defcomponent Root [data]
   (d/div {:id "root"}
          (ControlPanel data)
-         (Preview (get-record (:records data) (:selected-rec-name data)) Record)
+         (Preview (get-record (:records data) (:selected-rec data)) Record)
          ))
 
 (defn render [data]
