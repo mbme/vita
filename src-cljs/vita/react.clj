4a2d9e3b3e2204a65215b1ab717b8fd87d5c0502
mbme <stribog.ua@gmail.com>
2014-09-22 20:44:00 +0300

    added "displayName" to component

--- src-cljs/vita/react.clj
+++ src-cljs/vita/react.clj
@@ -3,5 +3,5 @@
 (defmacro defc [name args render & rest]
   `(def ~name
      (create-component
-      (hash-map :render (fn ~args ~render) ~@rest))
+      (hash-map :render (fn ~args ~render) :displayName '~name ~@rest))
      ))
