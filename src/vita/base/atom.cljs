fbdf4408712ee9574f2c9de2cfc9cd255104175b
mbme <stribog.ua@gmail.com>
2015-02-09 21:12:59 +0200

    properly serializing atom type

--- src/vita/base/atom.cljs
+++ src/vita/base/atom.cljs
@@ -3,6 +3,9 @@
 (defn str->keyword [str]
   (keyword (.substring str 1)))
 
+(defn keyword->str [keyword]
+  (str keyword))
+
 ;; Atom Info
 (defrecord AtomInfo [id type name])
 
@@ -12,10 +15,10 @@
 ;; Atom
 (defrecord VitaAtom [id type name data]
   Object
-  (toString [this] (str type "/" id "[" @name "]")))
+  (toString [this] (str "[" id "]" type "/" @name)))
 
 (defn json->atom [{:strs [id type name data]}]
   (->VitaAtom id (str->keyword type) (atom name) (atom data)))
 
 (defn atom->json [{:keys [id type name data]}]
-  {"id" id "type" type "name" @name "data" @data})
+  {"id" id "type" (keyword->str type) "name" @name "data" @data})
