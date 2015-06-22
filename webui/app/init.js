264eda4afb788a72124ad5bd0a58cfac5a9b3ef2
mbme <stribog.ua@gmail.com>
2015-06-22 21:09:47 +0300

    notemap->board

--- webui/app/init.js
+++ webui/app/init.js
@@ -2,13 +2,18 @@
 
 import 'base/radio.shim';
 
+import $ from 'jquery';
 import Radio from 'radio';
+import Marionette from 'marionette';
 
 import session from 'base/session';
 import Socket from 'base/socket';
 import NotesManager from 'base/notes-manager';
 
-import app from 'main/main';
+import SearchPanel from 'search-panel/view';
+import BoardView from 'board/view';
+import ModalsView from 'modals/view';
+
 
 let socket = new Socket(session.config.socketAddr);
 let notesManager = new NotesManager(session.notes, session.openNotes);
@@ -95,6 +100,30 @@ workspaceChannel.on('note:delete', function (id) {
 });
 
 // INIT Application
+
+let app = new Marionette.Application();
+
+app.addRegions({
+    'sidePanel': '#side-panel',
+    'main':      '#main',
+    'modals':    '#modals'
+});
+
+app.on('start', function () {
+    app.getRegion('sidePanel').show(new SearchPanel({collection: session.notes}));
+    app.getRegion('main').show(new BoardView({collection: session.openNotes}));
+    app.getRegion('modals').show(new ModalsView());
+
+    $(document).on({
+        'dragover': function (e) {
+            // without this drop event doesn't work in chrome
+            e.preventDefault();
+        },
+        'drop': function (e) {
+            e.preventDefault();
+        }
+    });
+});
 
 socket.connect().then(function () {
     console.log('websocket: connected');
