5a7ae73580ff8f93dc8b1a0e27555d4c1a16f97c
mbme <stribog.ua@gmail.com>
2015-10-19 23:43:51 +0300

    component to display relative time with auto-update

---
+++ web/app/components/common/time-ago.js
@@ -0,0 +1,32 @@
+import {createReactComponent} from 'viter/viter';
+import moment from 'moment';
+
+function formatDate(timestamp) {
+  return moment(timestamp * 1000).fromNow();
+}
+
+export default createReactComponent({
+  displayName: 'TimeAgo',
+
+  getInitialState () {
+    return {
+      date: this.getFormattedDate()
+    }
+  },
+
+  componentWillMount () {
+    this.updateInterval = setInterval(() =>  this.setState({ date: this.getFormattedDate()}), 60*1000);
+  },
+
+  componentWillUnmount () {
+    clearInterval(this.updateInterval);
+  },
+
+  getFormattedDate () {
+    return formatDate(this.props.timestamp);
+  },
+
+  render () {
+    return <time>{this.state.date}</time>
+  }
+})
--- web/app/components/search/item.js
+++ web/app/components/search/item.js
@@ -1,16 +1,16 @@
 import {createReactComponent, bus} from 'viter/viter';
-import Category from 'components/common/category';
-import moment from 'moment';
 import cx from 'classnames';
 
+import Category from 'components/common/category';
+import TimeAgo from 'components/common/time-ago';
+
 export default createReactComponent(function SearchItem ({note}) {
-  let date = moment(note.timestamp * 1000).fromNow();
-  let {id, selected, name, categories} = note;
+  let {id, selected, name, categories, timestamp} = note;
   return (
     <li className={cx("SearchItem", {'is-selected': selected})}
         onClick={() => bus.publish('note:open', id)}>
       <span className="name">{name}</span>
-      <time>{date}</time>
+      <TimeAgo timestamp={timestamp}/>
       &nbsp;|&nbsp;
       {categories.map(cat => <Category key={cat} name={cat} />)}
     </li>
