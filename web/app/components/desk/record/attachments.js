4f2c27dd4d754af5d35de936ff4f27ac6dcb2b8c
mbme <stribog.ua@gmail.com>
2015-10-24 22:19:28 +0300

    add file picker component

--- web/app/components/desk/record/attachments.js
+++ web/app/components/desk/record/attachments.js
@@ -3,6 +3,7 @@ import {curry} from 'lodash';
 import {basePath} from 'config';
 import {createReactComponent} from 'viter/viter';
 import Attachment from './attachment';
+import FilePicker from 'components/common/file-picker';
 
 let getFileAddr = curry(function (key, name) {
   return `${basePath}/notes/${key.type}/${key.id}/attachments/${name}`;
@@ -17,19 +18,26 @@ export default createReactComponent({
     let addr = getFileAddr(noteKey);
 
     return (
-      <table className="Attachments">
-        <tbody>
-          {attachments.map(attachment =>
-            <Attachment key={attachment.name}
-                        attachment={attachment}
-                        buildAddress={addr}
-                        onDelete={this.onDelete}/>)}
-        </tbody>
-      </table>
+      <div className="Attachments">
+        <FilePicker onFileSelected={this.selectFile}/>
+        <table>
+          <tbody>
+            {attachments.map(attachment =>
+              <Attachment key={attachment.name}
+                          attachment={attachment}
+                          buildAddress={addr}
+                          onDelete={this.onDelete}/>)}
+          </tbody>
+        </table>
+      </div>
     );
   },
 
   onDelete () {
     console.error('delete attachment');
+  },
+
+  selectFile (file) {
+    console.error('select file', file.name);
   }
 })
